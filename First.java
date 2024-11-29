d0b4ad93b2a2c35c5c65caee08759b47b47e352d
srikanth136 <73768553+srikanth136@users.noreply.github.com>
2024-11-29 15:06:28 +0530

    First

---
+++ First.java
@@ -0,0 +1,9 @@
+class First{
+    public static void main(String[] args){
+        int a = 10, b = 10, sum = 0, sub = 0, mul = 0;
+
+        //Addition 
+        System.out.println(a+b);
+
+    }
+}
